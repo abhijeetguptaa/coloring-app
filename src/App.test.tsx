9063f8c3c9836d863d1788d19cef5b2ffcd3e998
abhijeetguptaa <abhijeetguptaa@gmail.com>
2025-06-28 17:42:25 +0530

    CLeaning the repo

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders AI Coloring Books header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/AI Coloring Books/i);
-  expect(headerElement).toBeInTheDocument();
-});
